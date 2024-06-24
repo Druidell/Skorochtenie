2b92ca3c8036bd4e3cde6ef2dcef37fefd459a74
Druidell <danil.kochergin.1305@mail.ru>
2024-06-24 15:53:42 +0300

    Обновление и переименование Contacts.html в Contacts.php

--- Contacts.php
+++ Contacts.php
@@ -1,27 +1,36 @@
+<?php require_once("config.php");?> 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="Index.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="index.css">
+    <title>Контакты</title>
 </head>
 <body>
     <header>
         <nav>
+        <div class="content">
             <div class="navbar-nav">
                 <div class="navbar-nav-item">
-                    <a href="Index.html" class="sitename"><strong>GrandRead</strong></a>
-                    <a href="Index.html" class="nav-link">Главная</a>
-                    <a href="Catalog.html" class="nav-link">Каталог</a>
-                    <a href="Reviews.html" class="nav-link">Отзывы</a>
-                    <a href="Contacts.html" class="nav-link">Контакты</a>
-                    <a href="#4" class="login-link">Войти</a>
+                    <a href="Index.php" class="sitename"><strong>GrandRead</strong></a>
+                    <a href="Index.php" class="nav-link">Главная</a>
+                    <a href="Catalog.php" class="nav-link">Каталог</a>
+                    <a href="Reviews.php" class="nav-link">Отзывы</a>
+                    <a href="Contacts.php" class="nav-link">Контакты</a>
+                    <?php
+                    if ($_SESSION['login_sess']!='') { ?> 
+                    <a href="account.php" class="login-link">Личный кабинет</a>
+                    <?php } else { ?>
+                      <a href="account.php" class="login-link">Войти</a>
+                    <?php } ?>
               </div>
+          </div>
           </div>
         </nav>
     </header>
+    
     <div class="content">
     <h1 class="title">Контакты</h1>
     <div class="contacts-container">
@@ -31,15 +40,17 @@
         </div>
         <div class="social-contain">
             <p class="textone"><b>Встретимся в соц. сетях</b></p>
-            <img src="Picture/icon9.png">
-            <img src="Picture/icon10.png">
+            <a href="https://web.telegram.org"><img src="Picture/icon9.png"></a>
+            <a href="https://vk.com"><img src="Picture/icon10.png"></a>
         </div>
     </div>
+
+    <div class="content">
     <div class="question-container">
         <h1>Часто задаваемые вопросы:</h1>
         <details class="question-details">
-            <summary class="question-summary">В чем мне помогут онлайн-курсы?</summary>
-            <p>В чем мне помогут онлайн-курсы?</p>
+            <summary class="question-summary">Как долго будут доступны уроки?</summary>
+            <p>Как долго будут доступны уроки?</p>
         </details>
         <details class="question-details">
             <summary class="question-summary">В чем мне помогут онлайн-курсы?</summary>
@@ -58,13 +69,17 @@
             <p>Что мне понадобятся для занятий?</p>
         </details>
     </div>
+    </div>
+
+    <div class="content">
     <div class="contacts-container">
         <div class="social-contain">
             <p class="textone"><b>Остались вопросы?</b></p>
             <p class="textone"><b>Напишите нам в социальные сети</b></p>
-            <img src="Picture/icon9.png">
-            <img src="Picture/icon10.png">
+            <a href="https://web.telegram.org"><img src="Picture/icon9.png"></a>
+            <a href="https://vk.com"><img src="Picture/icon10.png"></a>
         </div>
+    </div>
     </div>
     </div>
 
@@ -73,7 +88,7 @@
             <div class="footer-container">
                 <div class="footer-text">
                     <p class="footer-name">&copy;GrandRead</p>
-                    <p><a href="Privacy.html">Политика конфиденциальности</a></p>
+                    <p><a href="Privacy.php">Политика конфиденциальности</a></p>
                     </div>
             </div>
             <div class="container">
@@ -93,6 +108,6 @@
             </div>
         </div>
     </footer>
-<script src="Index.js"></script>
+<script src="index.js"></script>
 </body>
 </html>
